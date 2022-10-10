682e56007bddba34458e65538a830a33906116e0
BVChamarthi <bharathvarma169@gmail.com>
2022-10-10 17:47:30 -0400

    set up splitter function

--- grid_splitter/grid_splitter.py
+++ grid_splitter/grid_splitter.py
@@ -28,7 +28,7 @@ class GridSplitter(DockWidget):
 
         # set up button to split the layer and save it
         self.splitButton = QPushButton('split', self)
-        self.splitButton.clicked.connect(self.debugPopup)
+        self.splitButton.clicked.connect(self.on_splitButton_clicked)
 
         # add GUI elements to docker
         self.mainWidget.setLayout(QFormLayout())
@@ -40,6 +40,8 @@ class GridSplitter(DockWidget):
         self.width = 0                  # width of open image
         self.height = 0                 # height of open image
         self.doc = None                 # document reference of open image
+        self.xRes = 72                  # x-resolution of image (pixels per inch)
+        self.yRes = 72                  # y-resolution of image (pixels per inch)
 
         # call a second set up function to finish the set up when the main window is fully loaded
         Krita.instance().notifier().windowCreated.connect(self.on_windowCreated)
@@ -57,13 +59,34 @@ class GridSplitter(DockWidget):
             self.wbox.setRange(0, 0)
             self.hbox.setRange(0, 0)
             self.doc = None
+            self.xRes = 72
+            self.yRes = 72
         
         else:
             self.doc = self.window.activeView().document()
-            self.width = self.doc.bounds().width()
-            self.height = self.doc.bounds().height()
+            self.width = self.doc.width()
+            self.height = self.doc.height()
             self.wbox.setRange(1, self.width)
             self.hbox.setRange(1, self.height)
+            self.xRes = self.doc.xRes()
+            self.yRes = self.doc.yRes()
+    
+    # function called when splitButton is clicked, splits the current active layer and saves the splits to given folder
+    def on_splitButton_clicked(self):
+        if len(self.window.views()) == 0:
+            self.popup('ERROR: no document is open')
+            return
+        
+        activeNode = self.doc.activeNode()
+
+        dirpath = QFileDialog.getExistingDirectory(None, 'Saves folder', '')
+        if dirpath == '':
+            self.popup('No seves folder selected, split aborted')
+            return
+        
+        basename = activeNode.uniqueId().toByteArray(1)
+
+        self.popup('on path to splitting image')
     
     # generic popup message function
     def popup(self, string: str):
